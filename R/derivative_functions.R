ba3440fcf72bb0a12c5482717a478195b3088507
tandethsquire <andy.iskauskas@gmail.com>
2021-02-04 14:06:36 +0000

    Added iteration.measure to directional_proposal to allow for expectation-based comparison of new points.

--- R/derivative_functions.R
+++ R/derivative_functions.R
@@ -54,7 +54,9 @@ directional_fit <- function(em, x, v, ...) {
 #'
 #' Provided a shared direction v has been identified, we then move in this direction as follows.
 #' The new point is defined as x + h*v, for some choice of h. To determine h, we initialise
-#' h = 0.1, propose a new point, and check the new implausibility. If the new implausibility
+#' h = 0.1, propose a new point, and check the new measure: the measures are either implausibility
+#' or (default) the difference between the target value and the emulator expectation, normalised
+#' by the target value. If the new measure
 #' is lower than the original one, we step along the direction further. If it is not, we reduce
 #' h and try again. This iteration terminates either when the new proposed point starts to
 #' increase the implausibility, if the value of h is particularly small (as determined by
@@ -65,12 +67,13 @@ directional_fit <- function(em, x, v, ...) {
 #' @param targets The list of emulator targets
 #' @param accept The implausibility value at which the proposal can increase the implausibility
 #' @param hcutoff A power of 10: the smallest allowed step-size, h
+#' @param iteration.measure Which measure to use for point suitability: expectation or implausibilty?
 #' @param iteration.steps How many iterations to perform before returning the point.
 #'
 #' @return A new proposal point, or the original point if a suitable new point could not be found.
 #'
 #' @export
-directional_proposal <- function(ems, x, targets, accept = 2, hcutoff = 1e-06, iteration.steps = 50) {
+directional_proposal <- function(ems, x, targets, accept = 2, hcutoff = 1e-06, iteration.measure = 'exp', iteration.steps = 50) {
   point_implaus <- purrr::map_dbl(seq_along(ems), ~ems[[.]]$implausibility(x, z = targets[[.]]))
   x_predict <- purrr::map_dbl(ems, ~.$get_exp(x))
   is_bigger <- purrr::map_lgl(seq_along(targets), ~targets[[.]]$val < x_predict[[.]])
@@ -78,29 +81,34 @@ directional_proposal <- function(ems, x, targets, accept = 2, hcutoff = 1e-06, i
   x_dir <- do.call('rbind', purrr::map(seq_along(is_bigger), ~if(is_bigger[[.]]) -1*x_diffs[.,] else x_diffs[.,]))
   x_norms <- apply(x_dir, 1, function(y) sqrt(sum(y^2)))
   x_dir <- sweep(x_dir, 1, x_norms, "/")
-  test_dirs <- runifs(1000, length(x))
+  test_dirs <- runifs(500 * length(x), length(x))
   suits <- apply(x_dir, 1, function(y) apply(test_dirs, 1, function(z) z %*% y))
   suit_means <- apply(suits, 1, mean)
   order_dirs <- test_dirs[order(suit_means, decreasing = TRUE),]
   order_suits <- suits[order(suit_means, decreasing = TRUE),]
   restrict_dirs <- order_dirs[apply(order_suits, 1, function(y) all(y >= 0 || point_implaus < accept)),]
+  nth_discrepancy <- function(ems, x, targets, n = 1) {
+    discs <- purrr::map_dbl(seq_along(ems), ~abs((ems[[.]]$get_exp(x)-targets[[.]]$val)/targets[[.]]$val))
+    if (n == 1) return(max(discs))
+    else return(order(discs, decreasing = TRUE)[[n]])
+  }
   if (nrow(restrict_dirs) == 0) {
     warning("No direction reduces all relevant targets.")
     return(x)
   }
   range_dists <- purrr::map_dbl(ems[[1]]$ranges, ~(.[[2]]-.[[1]])/2)
   best_dir <- restrict_dirs[1,] * range_dists
-  old_imp <- max(point_implaus)
+  old_measure <- if (iteration.measure == "exp") nth_discrepancy(ems, x, targets) else max(point_implaus)
   better_pt <- NULL
   attempts <- 0
   gap <- 0.1
   index <- 1
   while(attempts < 50) {
     new_point <- x + gap * index * best_dir
-    nimp <- nth_implausible(ems, new_point, targets)
-    if (nimp < old_imp) {
+    new_measure <- if (iteration.measure == "exp") nth_discrepancy(ems, new_point, targets) else nth_implausible(ems, new_point, targets)
+    if (new_measure < old_measure) {
       better_pt <- new_point
-      old_imp <- nimp
+      old_measure <- new_measure
       index <- index + 1
     }
     else {
